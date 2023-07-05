1c83996fce60c717d082e46f70ce35290b7dd65d
Safaie <hadisehsafaei.73@gmail.com>
2023-07-05 11:21:06 +0330

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GuessTheFlagMaPackage
+
+A description of this package.
---
+++ Sources/GuessTheFlagMaPackage/GuessTheFlagMaPackage.swift
@@ -0,0 +1,6 @@
+public struct GuessTheFlagMaPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
